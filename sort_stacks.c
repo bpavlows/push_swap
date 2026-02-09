b6d1d6416472d9b329e93a9726fcd05db4b41910
Betania Pavlowski De Quevedo <bpavlows@c2r1s6.42porto.com>
2026-02-09 11:16:22 +0000

    fix errors

--- sort_stacks.c
+++ sort_stacks.c
@@ -12,17 +12,26 @@
 
 #include "push_swap.h"
 
+static void	move_b_to_a(t_stack **a, t_stack **b)
+{
+	prep_for_push(a, (*b)->target_node, 'a');
+	pa(a, b);
+}
+
+static void	min_on_top(t_stack **a)
+{
+	t_stack	*min_node;
+
+	min_node = find_min(*a);
+	prep_for_push(a, min_node, 'a');
+}
+
 void	sort_stacks(t_stack **a, t_stack **b)
 {
 	int		len_a;
-	t_stack	*min_node;
 
 	len_a = stack_len(*a);
-	if (len_a-- > 3 && !stack_sorted(*a))
-		pb(b, a);
-	if (len_a-- > 3 && !stack_sorted(*a))
-		pb(b, a);
-	while (len_a-- > 3 && !stack_sorted(*a))
+	while (stack_len(*a) > 3 && !stack_sorted(*a))
 	{
 		init_nodes_a(*a, *b);
 		move_a_to_b(a, b);
@@ -31,10 +40,8 @@ void	sort_stacks(t_stack **a, t_stack **b)
 	while (*b)
 	{
 		init_nodes_b(*a, *b);
-		prep_for_push(a, (*b)->target_node, 'a');
-		pa(a, b);
+		move_b_to_a(a, b);
 	}
 	current_index(*a);
-	min_node = find_min(*a);
-	prep_for_push(a, min_node, 'a');
+	min_on_top(a);
 }
